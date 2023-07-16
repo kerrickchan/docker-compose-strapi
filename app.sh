9e32a55a72bbc74209e9cad5aab816495f13e870
Kerrick Chan <kerrickchan@icloud.com>
2023-07-16 18:18:29 +0800

    script add cp to app folder

--- app.sh
+++ app.sh
@@ -1,1 +1,5 @@
 yarn create-strapi-app app --dbclient=postgres --dbhost=strapiDB --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
+cp dockerfile app/dockerfile
+cp docker-compose.yml app/docker-compose.yml
+cd app
+docker-compose up --build -d
