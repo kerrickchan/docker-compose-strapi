fbf94e3ed274b7d522be100dc4f9f5b258defd73
Kerrick Chan <kerrickchan@icloud.com>
2023-07-16 17:47:04 +0800

    add nas specify

--- app.sh
+++ app.sh
@@ -1,1 +1,1 @@
-yarn create strapi-app app --dbclient=postgres --dbhost=strapiDB --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
+yarn create-strapi-app app --dbclient=postgres --dbhost=strapiDB --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
