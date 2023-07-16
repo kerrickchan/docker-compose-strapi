8e627d2bf6744a5409da842351d0689992909a4a
Kerrick Chan <kerrickchan@icloud.com>
2023-07-16 18:27:58 +0800

    fix permission problem

--- app.sh
+++ app.sh
@@ -1,4 +1,5 @@
 yarn create-strapi-app app --dbclient=postgres --dbhost=strapiDB --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
+chmod -R 777 app
 cp dockerfile app/dockerfile
 cp docker-compose.yml app/docker-compose.yml
 cd app
