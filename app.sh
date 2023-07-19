c665fff3fb0efaf9a7fc194e875bb62949b0500a
Kerrick Chan <kerrickchan@icloud.com>
2023-07-20 04:01:08 +0800

    change app folder name to keep project name sync

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
-yarn create-strapi-app app --dbclient=postgres --dbhost=strapiDB --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
-chmod -R 777 app
-cp dockerfile app/dockerfile
-cp docker-compose.yml app/docker-compose.yml
-cd app
+yarn create-strapi-app strapi --dbclient=postgres --dbhost=strapiDB --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
+chmod -R 777 strapi
+cp dockerfile strapi/dockerfile
+cp docker-compose.yml strapi/docker-compose.yml
+cd strapi
 docker-compose up --build -d
