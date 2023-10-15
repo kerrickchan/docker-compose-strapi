5551d1bab10ce776af80b9441fb228ed036275d9
Kerrick Chan <kerrickchan@icloud.com>
2023-10-15 18:42:40 +0800

    to bun. upgrade Strapi to v4.14.4

---
+++ create.sh
@@ -0,0 +1,6 @@
+bunx create-strapi-app strapi --dbclient=postgres --dbhost=strapiDB --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
+chmod -R 777 strapi
+cp dockerfile strapi/dockerfile
+cp docker-compose.yml strapi/docker-compose.yml
+cd strapi
+docker-compose up --build -d
