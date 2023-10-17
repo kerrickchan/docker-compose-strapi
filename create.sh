72a4220910779079ff6775eb84ff65eb1545a300
Kerrick Chan <kerrickchan@icloud.com>
2023-10-17 17:10:33 +0800

    specify strap version to v4.12.7

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
-yarn create strapi-app strapi --dbclient=postgres --dbhost=strapiDB --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
+npx create-strapi-app@v4.12.7 strapi --dbclient=postgres --dbhost=strapiDB --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
 chmod -R 777 strapi
 cp dockerfile strapi/dockerfile
 cp docker-compose.yml strapi/docker-compose.yml
