333c3b1c5891619ddb50f84c958b974ef498da7a
Kerrick Chan <kerrickchan@icloud.com>
2023-10-18 21:25:05 +0800

    change to local script start

--- create.sh
+++ create.sh
@@ -1,3 +1,2 @@
-bunx create-strapi-app@v4.14.4 strapi --dbclient=postgres --dbhost=strapiDB --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
-cp docker-compose-postgres.yml strapi/docker-compose-postgres.yml
+bunx create-strapi-app@v4.14.4 strapi --dbclient=postgres --dbhost=localhost --dbport=5432 --dbname=strapi --dbusername=strapi --dbpassword=strapi --dbssl=false --no-run --typescript
 docker-compose up --build -d
---
+++ start.sh
@@ -0,0 +1,2 @@
+cd strapi
+bun develop
